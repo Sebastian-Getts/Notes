ca72e20414b6b651fd2d2c96e0df53850603886d
Sebastian <2573992956@qq.com>
2020-01-04 12:24:48 +0800

    update from Ubuntu

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/biin/bash
-hexo clean && hexo g && hexo douban -m  && hexo deploy && git add -A && git commit -m "update" && git push origin source
+hexo clean && hexo g && hexo douban -m  && hexo deploy && git add -A && git commit -m "update from Ubuntu" && git push origin source
