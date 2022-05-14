7e56c5d52ad126cead737c1bfd4ecbf37f255a28
seabstian <zzzqsw@gmail.com>
2022-05-14 14:44:00 +0800

    update from Mac

--- push.sh
+++ push.sh
@@ -1,2 +1,2 @@
 #!/biin/bash
-hexo clean && hexo g && hexo deploy && git add -A && git commit -m "update from Ubuntu" && git push origin source
+hexo clean && hexo g && hexo deploy && git add -A && git commit -m "update from Mac" && git push origin source
